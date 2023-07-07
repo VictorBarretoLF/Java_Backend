7ebd7e3c6ffb9587a50f069bae60f827d8a9e97c
Víctor Barreto <victor.barreto@behoh.com>
2023-07-07 17:49:52 -0300

    inicializando projeto

---
+++ cursospringrestapi/README.md
@@ -0,0 +1,1 @@
+docker run --name postgres -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=teste POSTGRES_DB=curso-api-rest-spring -p 5432:5432 -d postgres:12
--- cursospringrestapi/src/main/java/curso/api/rest/CursospringrestapiApplication.java
+++ cursospringrestapi/src/main/java/curso/api/rest/CursospringrestapiApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -14,7 +13,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EntityScan(basePackages = {"curso.api.rest.model"})
 @ComponentScan(basePackages = {"curso.*"})
 @EnableJpaRepositories(basePackages = {"curso.api.rest.repository"})
-@EnableTransactionManagement
 @EnableWebMvc
 @RestController
 @EnableAutoConfiguration
