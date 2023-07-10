67cf1a654699af9475ecb9e978e9650b8a9aca1b
Víctor Barreto <victor.barreto@behoh.com>
2023-07-10 11:19:16 -0300

    customizando métodos RESTful e URLs

--- cursospringrestapi/README.md
+++ cursospringrestapi/README.md
@@ -1,1 +1,3 @@
-docker run --name postgres -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=teste POSTGRES_DB=curso-api-rest-spring -p 5432:5432 -d postgres:12
+```bash
+	docker run --name postgres -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=teste POSTGRES_DB=curso-api-rest-spring -p 5432:5432 -d postgres:12
+```
--- cursospringrestapi/src/main/java/curso/api/rest/controller/IndexController.java
+++ cursospringrestapi/src/main/java/curso/api/rest/controller/IndexController.java
@@ -1,13 +1,19 @@
 package curso.api.rest.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import curso.api.rest.model.Usuario;
+import curso.api.rest.repository.UsuarioRepository;
 
 @RestController
 @RequestMapping(value = "/usuario")
@@ -36,17 +42,22 @@ public class IndexController {
 	}
 	*/
 	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@GetMapping(value = "/{id}", produces = "application/json")
+	public ResponseEntity<Usuario> init(@PathVariable (value = "id") Long id) {
+		
+		Optional<Usuario> usuario = usuarioRepository.findById(id);
+		
+		return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK);
+	}
+	
 	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity<Usuario> init() {
+	public ResponseEntity<List<Usuario>> usuario() {
 		
-		Usuario usuario = new Usuario();
-		usuario.setId(50L);
-		usuario.setLogin("victor@gmail.com");
-		usuario.setNome("Víctor");
-		usuario.setSenha("123456");
+		List<Usuario> list = (List<Usuario>) usuarioRepository.findAll();
 		
-		// Cria uma nova ResponseEntity com uma mensagem contendo o 'nome' e 'salario',
-		// juntamente com o status HTTP OK (200).
-		return ResponseEntity.ok(usuario);
+		return new ResponseEntity<List<Usuario>>(list, HttpStatus.OK);
 	}
 }
