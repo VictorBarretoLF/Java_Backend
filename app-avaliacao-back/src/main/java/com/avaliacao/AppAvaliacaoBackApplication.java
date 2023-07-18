d29eaf02804b4fa8a079ab8fb3dc30299c2e7928
Víctor Barreto <victor.barreto@behoh.com>
2023-07-18 17:10:08 -0300

    feat: inicializando projeto

---
+++ app-avaliacao-back/src/main/java/com/avaliacao/AppAvaliacaoBackApplication.java
@@ -0,0 +1,13 @@
+package com.avaliacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAvaliacaoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppAvaliacaoBackApplication.class, args);
+	}
+
+}
