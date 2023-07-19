5734ecd78932c326c7550c69c32e7443396f471c
Víctor Barreto <victorbarretolins@gmail.com>
2023-07-18 22:58:58 -0300

    feat: adicionado seed sql para gerar dados mockados no banco h2

--- catalago-de-produtos/src/main/java/com/catalogoprodutos/services/CategoryService.java
+++ catalago-de-produtos/src/main/java/com/catalogoprodutos/services/CategoryService.java
@@ -2,8 +2,11 @@ package com.catalogoprodutos.services;
 
 import java.util.List;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.catalogoprodutos.entities.Category;
 import com.catalogoprodutos.repositories.CategoryRepository;
@@ -14,6 +17,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository categoryRepository;
 
+	@Transactional(readOnly = true)
 	public List<Category> findAll(){
 		return categoryRepository.findAll();
 	}
