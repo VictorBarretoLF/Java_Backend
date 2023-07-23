0f67e9c9e61cacf9e04d1a13e25d5abd1b3ee205
Víctor Barreto <victorbarretolins@gmail.com>
2023-07-23 19:07:13 -0300

    feat: criado controler e service para retornar todos os controllers

--- product-service/src/main/java/com/productservice/service/ProductService.java
+++ product-service/src/main/java/com/productservice/service/ProductService.java
@@ -1,9 +1,12 @@
 package com.productservice.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.productservice.dto.ProductRequest;
+import com.productservice.dto.ProductResponse;
 import com.productservice.model.Product;
 import com.productservice.repository.ProductRepository;
 
@@ -25,5 +28,20 @@ public class ProductService {
 		
 		productRepository.save(product);
 		log.info("Product {} is saved", product.getId());
+	}
+	
+	public List<ProductResponse> getAllProducts() {
+		List<Product> products = productRepository.findAll();
+		
+		return products.stream().map(product -> mapToProductResponse(product)).toList();
+	}
+	
+	private ProductResponse mapToProductResponse(Product product) {
+		return ProductResponse.builder()
+				.id(product.getId())
+				.name(product.getName())
+				.description(product.getDescription())
+				.price(product.getPrice())
+				.build();
 	}
 }
