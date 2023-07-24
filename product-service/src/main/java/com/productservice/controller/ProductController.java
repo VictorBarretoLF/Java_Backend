79efb6f36844744e718e708a8b5fa248bd09089c
Víctor Barreto <victor.barreto@behoh.com>
2023-07-24 18:05:57 -0300

    feat: adicionado testes em mockMvc

--- product-service/src/main/java/com/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/productservice/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.productservice.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import com.productservice.service.ProductService;
 @RequestMapping("/product")
 public class ProductController {
 	
+	@Autowired
 	private ProductService productService;
 	
 	@PostMapping
