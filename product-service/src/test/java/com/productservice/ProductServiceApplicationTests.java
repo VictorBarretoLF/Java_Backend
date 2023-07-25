72d914bc70b979c2267f938baec43e2596422a1a
Víctor Barreto <victorbarretolins@gmail.com>
2023-07-24 21:06:22 -0300

    feat: criado teste de MockMvc para testar o controller do get

--- product-service/src/test/java/com/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/productservice/ProductServiceApplicationTests.java
@@ -4,6 +4,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.math.BigDecimal;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -16,9 +17,14 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.productservice.dto.ProductRequest;
+import com.productservice.repository.ProductRepository;
 
 @SpringBootTest
 @Testcontainers
@@ -33,6 +39,9 @@ class ProductServiceApplicationTests {
 	
 	@Autowired
 	private ObjectMapper objectMapper;
+	
+	@Autowired
+	private ProductRepository productRepository;
 	
 	@DynamicPropertySource
 	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
@@ -49,6 +58,35 @@ class ProductServiceApplicationTests {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(productRequestString))
         		.andExpect(status().isCreated());
+        Assertions.assertEquals(1, productRepository.findAll().size());
+	}
+	
+	@Test
+	void shouldGetAllProducts() throws Exception {
+	    // First, create some products to test with
+	    ProductRequest productRequest1 = getProductRequest();
+	    String productRequestString1 = objectMapper.writeValueAsString(productRequest1);
+
+	    mockMvc.perform(MockMvcRequestBuilders.post("/product")
+	            .contentType(MediaType.APPLICATION_JSON)
+	            .content(productRequestString1))
+	            .andExpect(status().isCreated());
+
+	    ProductRequest productRequest2 = getProductRequest();
+//	    String productRequestString2 = objectMapper.writeValueAsString(productRequest2);
+//
+//	    mockMvc.perform(MockMvcRequestBuilders.post("/product")
+//	            .contentType(MediaType.APPLICATION_JSON)
+//	            .content(productRequestString2))
+//	            .andExpect(status().isCreated());
+
+	    // Then, make a GET request to the /product endpoint
+	    mockMvc.perform(MockMvcRequestBuilders.get("/product")
+	            .contentType(MediaType.APPLICATION_JSON))
+	            .andExpect(status().isOk())
+	            .andExpect(jsonPath("$", hasSize(2))) // I should expects 3 if you uncomment the code above
+	            .andExpect(jsonPath("$[0].name", is("Iphone 20"))) // Assert that the name of the first product is correct
+	            .andExpect(jsonPath("$[1].name", is("Iphone 20"))); // Assert that the name of the second product is correct
 	}
 
 	private ProductRequest getProductRequest() {
